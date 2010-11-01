246e6e8f71198d1716f91386fb4adc4e45fd4d27
BuZZ-dEE <deezzub@gmail.com>
2010-11-01 11:21:34 +0100

    nothing concrete

--- src/btree/BTree.java
+++ src/btree/BTree.java
@@ -1,5 +1,7 @@
 package btree;
 
+import java.util.Collections;
+
 /**
  * Die Implementierung eines B-Baums f&uuml;r Integerwerte mit den Operationen
  * Einf&uuml;gen und Suchen
@@ -10,16 +12,45 @@ package btree;
  */
 public class BTree implements IBTree {
 
+	public int order = 4;
+	public Node root;
+	public Node child;
+	public BTree btree;
+	
 	@Override
 	public boolean contains(int key) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		boolean exists = false;
+//		Iterator<Integer> iter = root.iterator(); 
+//		
+//		for (Integer integer: root) {
+//			if (root.contains(key) && integer.intValue() == key) {
+//				System.out.println(root.indexOf(integer));
+//			} else {
+//				if (iter.next() > key) {
+//					
+//				}
+//			}
+//		}
+		
+		if (root.indexOf(key) >= 0) {
+			System.out.println(root.indexOf(key));
+		} else {
+			for (Integer integer: root) {
+				if (integer > key) {
+					child = root.getChildren().get(root.indexOf(key));
+					child.contains(key);
+				}
+			}
+		}
+		
+		return exists;
 	}
 
 	@Override
 	public void insert(int key) {
-		// TODO Auto-generated method stub
-		
+		//Node node = new Node();
+		Collections.sort(node);
 	}
 
 }
--- src/btree/Node.java
+++ src/btree/Node.java
@@ -1,8 +1,55 @@
 package btree;
 
-public class Node {
+import java.util.ArrayList;
 
-	public Node() {
-		
+/**
+ * Die Klasse Node repräsentiert einen Knoten eines B-Baums
+ * 
+ * @author buzz-dee
+ *
+ */
+public class Node extends ArrayList<Integer> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public int min_size;
+	public int max_size;
+	//public ArrayList<Integer> keys;
+	public Node father;
+	public ArrayList<Node> children;
+	
+	public Node(int min_size, int max_size) {
+		this.min_size = min_size;
+		this.max_size = max_size;
+	}
+
+	/**
+	 * @return the father
+	 */
+	public Node getFather() {
+		return father;
+	}
+
+	/**
+	 * @param father the father to set
+	 */
+	public void setFather(Node father) {
+		this.father = father;
+	}
+
+	/**
+	 * @return the children
+	 */
+	public ArrayList<Node> getChildren() {
+		return children;
+	}
+
+	/**
+	 * @param children the children to set
+	 */
+	public void setChildren(ArrayList<Node> children) {
+		this.children = children;
 	}
 }
